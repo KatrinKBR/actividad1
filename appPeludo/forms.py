8e1d6069b2f5c45464ee02e0f5afc097b68eef9d
KatrinKBR <ka.bethencourt@duocuc.cl>
2021-06-16 14:23:01 -0400

    "Algunos cambios"

--- appPeludo/forms.py
+++ appPeludo/forms.py
@@ -3,6 +3,7 @@ from django.forms import ModelForm, fields, widgets
 from .models import Mascota
 
 GENERO = [('1','---'),('Hembra','Hembra'),('Macho','Macho')]
+
 ESPECIE = [('1','---'),('Perro','Perro'),('Gato','Gato')]
 
 class MascotaForm(ModelForm):
@@ -20,6 +21,7 @@ class MascotaForm(ModelForm):
         }
 
 class MascotaFormMod(ModelForm):
+    disabled_fields = ['nro_chip']
     class Meta:
         model = Mascota
         fields=['nro_chip','nombre','genero','edad','especie','esterilizado']
@@ -31,4 +33,12 @@ class MascotaFormMod(ModelForm):
             'edad': forms.TextInput(attrs={'class':'form-control'}),
             'especie': forms.Select(attrs={'class':'form-select'},choices=ESPECIE),
             'esterilizado': forms.CheckboxInput(attrs={'class':'form-check-input'})
-        }        
+        }
+
+        # def clean_nro_chip(self):
+        #     nro_chip = self.cleaned_data.get('nro_chip')
+        #     try:
+        #         match = Mascota.objects.get(nro_chip=nro_chip)
+        #         raise forms.ValidationError("nro_chip ya existe")
+        #     except Mascota.DoesNotExist:
+        #         return nro_chip
