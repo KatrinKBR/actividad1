b4df8d8717acae9dbf1a1d6527ab923e741ab10b
KatrinKBR <ka.bethencourt@duocuc.cl>
2021-06-23 14:29:59 -0400

    "Añadiendo seguridad a la API"

--- rest_mascota/urls.py
+++ rest_mascota/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rest_mascota.views import *
+from rest_mascota.viewsLogin import login
 
 urlpatterns = [
     path('lista_mascota', lista_mascota, name='lista_mascota'),
-    path('detalle_mascota/<str:nro_chip>/', detalle_mascota, name='detalle_mascota')
+    path('detalle_mascota/<str:nro_chip>/', detalle_mascota, name='detalle_mascota'),
+    path('login', login, name="login"),
 ]
